29f638d0b7061d390da5eed2a5fdca1079819d0c
zxlegend-blip <75721111+zxlegend-blip@users.noreply.github.com>
2021-03-31 09:58:17 +0530

    Add files via upload

---
+++ PROJECT-MONKEY-GO-HAPPY-master/PROJECT-MONKEY-GO-HAPPY-master/sketch.js
@@ -0,0 +1,105 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var FoodGroup, bananaImage;
+
+var gameOver;
+var score=0;
+
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  
+  
+
+  bananaImage = loadImage("Banana.png");
+  
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+  FoodGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  
+  background(255);
+  
+    
+  if(ground.x<0) {
+    ground.x=ground.width/2;
+  }
+  if(backgr.x<100){
+    backgr.x=backgr.width/2;
+  }
+  
+    if(FoodGroup.isTouching(player)){
+      FoodGroup.destroyEach();
+    score = score + 2;
+    }
+    switch(score){
+        case 10: player.scale=0.12;
+                break;
+        case 20: player.scale=0.14;
+                break;
+        case 30: player.scale=0.16;
+                break;
+        case 40: player.scale=0.18;
+                break;
+        default: break;
+    }
+  
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+    spawnFood();
+ 
+   
+  
+  drawSprites();
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+ score, 500,50);
+}
+
+function spawnFood() {
+  //write code here to spawn the food
+  if (frameCount % 80 === 0) {
+    var banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);    
+    banana.addImage(bananaImage);
+    banana.scale = 0.05;
+    banana.velocityX = -5;
+     //assign lifetime to the variable
+    banana.lifetime = 300;
+    player.depth = banana.depth + 1;
+    
+    //add each banana to the group
+    FoodGroup.add(banana);
+  }
+}
